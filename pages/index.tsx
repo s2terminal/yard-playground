fe9e6e6f17f7f8eb1dc6029bc5af94c4e6218845
suzuki.sh <suzuki.sh@s2terminal.com>
2019-06-24 01:58:30 +0900

    remove unnecessary code from YARD HTML

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button, Grid  } from '@material-ui/core';
 import * as React from 'react';
 import fetch from 'isomorphic-unfetch';
+import cheerio from 'cheerio';
 
 const defaultRuby = `# @param [String] name ユーザ名
 # @return [String] password あいさつ
@@ -32,8 +33,7 @@ class Index extends React.Component<any, State> {
       mode: 'no-cors',
       body: data
     });
-
-
+    this.setYard();
   }
 
   handleChange(event) {
@@ -45,7 +45,9 @@ class Index extends React.Component<any, State> {
   async setYard() {
     const res = await fetch(`${sinatraOrigin}/yard`);
     const yard = await res.text();
-    document.getElementById('iframe-yard').srcdoc = yard;
+    const $ = cheerio.load(yard);
+
+    document.getElementById('iframe-yard').srcdoc = $('#content');
   }
 
   render() {
@@ -55,20 +57,27 @@ class Index extends React.Component<any, State> {
         <form>
           <div>
             <Button variant="contained" color="primary" onClick={this.handleSubmit}>
-              Submit!
+              Generate YARD Document!
             </Button>
           </div>
-          <TextField
-            id="textfield-ruby"
-            name="ruby"
-            label="Ruby Source Code"
-            multiline
-            style={ {width: "640px"} }
-            value={this.state.ruby}
-            margin="normal"
-            onChange={this.handleChange}
-          />
-          <iframe id="iframe-yard" width="640" height="480"></iframe>
+          <Grid container>
+            <Grid item xs={6}>
+              <TextField
+                id="textfield-ruby"
+                name="ruby"
+                label="Ruby Source Code"
+                multiline
+                style={ {width: "90%"} }
+                value={this.state.ruby}
+                margin="normal"
+                onChange={this.handleChange}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <iframe id="iframe-yard" height="480" style={ {width: "90%"} }></iframe>
+            </Grid>
+          </Grid>
+
         </form>
         <p>
           <a href="https://github.com/lsegal/yard">lsegal/yard: YARD is a Ruby Documentation tool. The Y stands for &quot;Yay!&quot;</a>
