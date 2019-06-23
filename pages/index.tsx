2c817c10c67d04040279eb9b0d0bf152bdecb61e
suzuki.sh <suzuki.sh@s2terminal.com>
2019-06-24 02:29:57 +0900

    fix header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,24 @@
-import { TextField, Button, Grid  } from '@material-ui/core';
+import { TextField, Button, Grid, AppBar, Toolbar, Typography } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import * as React from 'react';
 import fetch from 'isomorphic-unfetch';
 import cheerio from 'cheerio';
 
-const defaultRuby = `# @param [String] name ユーザ名
-# @return [String] password あいさつ
+const defaultRuby = `# あいさつをします
+#
+# @param [String] name ユーザ名
+# @return [String] あいさつ
 def hello(name)
   return "こんにちは、#{name}さん"
 end
-`;
+
+# (see #hello)
+# @note このメソッド使わないほうが良い
+def regacyHello(name)
+  self.state += 1
+  "hello"
+end`;
 const sinatraOrigin = 'http://localhost:4567';
 
 interface State {
@@ -53,32 +63,37 @@ class Index extends React.Component<any, State> {
   render() {
     return (
       <>
-        <h1>YARD Playground</h1>
-        <form>
-          <div>
+        <CssBaseline />
+        <Grid container>
+          <Grid item xs={12} style={{marginBottom: "1rem"}}>
+            <AppBar position="static">
+              <Toolbar>
+                <Typography variant="h6" color="inherit">
+                  YARD Playground
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="textfield-ruby"
+              name="ruby"
+              label="Ruby Source Code"
+              multiline
+              style={ {width: "90%", marginLeft: "1rem"} }
+              value={this.state.ruby}
+              margin="normal"
+              onChange={this.handleChange}
+            />
             <Button variant="contained" color="primary" onClick={this.handleSubmit}>
               Generate YARD Document!
             </Button>
-          </div>
-          <Grid container>
-            <Grid item xs={6}>
-              <TextField
-                id="textfield-ruby"
-                name="ruby"
-                label="Ruby Source Code"
-                multiline
-                style={ {width: "90%"} }
-                value={this.state.ruby}
-                margin="normal"
-                onChange={this.handleChange}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <iframe id="iframe-yard" height="480" style={ {width: "90%"} }></iframe>
-            </Grid>
           </Grid>
+          <Grid item xs={6}>
+            <iframe id="iframe-yard" height="480" style={ {width: "90%"} }></iframe>
+          </Grid>
+        </Grid>
 
-        </form>
         <ul>
           <li>about YARD: <a target="blank" href="https://github.com/lsegal/yard">lsegal/yard: YARD is a Ruby Documentation tool. The Y stands for &quot;Yay!&quot;</a></li>
           <li>about this source code: <a target="blank" href="https://github.com/s2terminal/yard-playground">s2terminal/yard-playground</a></li>
